f5c123d4ed59915ae37f70e3bc12d52db48398f4
Rrebirrth <davidmb2018@gmail.com>
2019-06-27 16:12:48 +0200

    Added fields for first name and last name to the registration form

--- users/forms.py
+++ users/forms.py
@@ -6,15 +6,18 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserRegisterForm(UserCreationForm):
     email = forms.EmailField()
+    first_name = forms.CharField(max_length=15, label='First name')
+    last_name = forms.CharField(max_length=15, label='Last name')
     
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1','password2' ]
+        fields = ['username', 'email','first_name','last_name', 'password1','password2' ]
 
 
 
-
+class UserUpdateFrom(forms.ModelForm):
+    pass
 
 
 
