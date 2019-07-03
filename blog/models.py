d9e3badb9de5401e75dff6efce74fc461afd9adf
Rrebirrth <davidmb2018@gmail.com>
2019-07-03 14:49:46 +0200

    Added facilities for non-admin users to create, update and delete posts.

--- blog/models.py
+++ blog/models.py
@@ -2,18 +2,20 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django import forms
+from django.urls import reverse
 
 Topic_CHOICES= [
-    ('politics', 'Politics'),
-    ('philosophy', 'Philosophy'),
-    ('sports', 'Sports'),
-    ('literature', 'Literature'),
-    ('tech', 'Tech'),
-    ('physics', 'Physics'),
-    ('biology', 'Biology'),
-    ('chemistry', 'Chemistry'),
-    ('mathematics', 'Mathematics'),
-    ('identity', 'Identity'),
+    ('Current Eevents','Current Events'),
+    ('Politics', 'Politics'),
+    ('Philosophy', 'Philosophy'),
+    ('Sports', 'Sports'),
+    ('Literature', 'Literature'),
+    ('Tech', 'Tech'),
+    ('Physics', 'Physics'),
+    ('Biology', 'Biology'),
+    ('Chemistry', 'Chemistry'),
+    ('Mathematics', 'Mathematics'),
+    ('Identity', 'Identity'),
     ]
 
 Language_CHOICES= [
@@ -43,13 +45,17 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    topic = models.CharField(max_length=15, choices=Topic_CHOICES)
-    launguage = models.CharField(max_length=15, choices=Language_CHOICES, default = 'English')
+    topic = models.CharField(max_length=15, choices=Topic_CHOICES, default='Current Events')
+    language = models.CharField(max_length=25, choices=Language_CHOICES, default = 'English')
 
 
 
     def __str__(self):
+              
         return self.title
 
+    def get_absolute_url(self):
+        return reverse("post-detail", kwargs={"pk": self.pk})
+    
 
 
