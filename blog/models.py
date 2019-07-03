ae434f947621e519dfa12c91a03a338c5dfaa1b6
Rrebirrth <davidmb2018@gmail.com>
2019-07-03 13:23:24 +0200

    Added the topic and language fields to the Post model

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,41 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django import forms
+
+Topic_CHOICES= [
+    ('politics', 'Politics'),
+    ('philosophy', 'Philosophy'),
+    ('sports', 'Sports'),
+    ('literature', 'Literature'),
+    ('tech', 'Tech'),
+    ('physics', 'Physics'),
+    ('biology', 'Biology'),
+    ('chemistry', 'Chemistry'),
+    ('mathematics', 'Mathematics'),
+    ('identity', 'Identity'),
+    ]
+
+Language_CHOICES= [
+    (' Chinese', ' Chinese'),
+    ('Spanish', 'Spanish'),
+    ('English', 'English'),
+    ('Hindi', 'Hindi'),
+    ('Arabic', 'Arabic'),
+    ('Portuguese', 'Portuguese'),
+    ('Bengali', 'Bengali'),
+    ('Russian', 'Russian'),
+    ('Japanese', 'Japanese'),
+    ('Punjabi', 'Punjabi'),
+    ('Norwegian', 'Norwegian'),
+    ('German', 'German'),
+    ('French', 'French'),
+    ('Dutch', 'Dutch'),
+        ]
+
+
+
+#forms.CharField(label='Topic', widget=forms.Select(choices=Topic_CHOICES))
 
 
 class Post(models.Model):
@@ -8,6 +43,10 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    topic = models.CharField(max_length=15, choices=Topic_CHOICES)
+    launguage = models.CharField(max_length=15, choices=Language_CHOICES, default = 'English')
+
+
 
     def __str__(self):
         return self.title
