fea9091e7935ab954170e0fda112875b86474ecf
rrebirrth <davidmb2018@gmail.com>
2019-07-19 20:40:46 +0800

    Updated requirements and collected statics to statics folder in home dir

--- OpenSourceNews/settings.py
+++ OpenSourceNews/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '4^dk8@n#li@f+48tlt@0u^p+%m=9$4n$8^^glsope@+p-5^-!w'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['avantgaurdian.pythonanywhere.com']
 
 
 # Application definition
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/Demo_NewsApp/static'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
